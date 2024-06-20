018459d7c6263a0f444f50d16d7b47f3e454aa8c
RustyRyan4Real <damien.paszkiewicz@live.fr>
2024-06-20 16:11:53 +0200

    Maj (CV + portfolio)
    Mise à jour du cv
    1ere version du portfolio

---
+++ vanilla-script.js
@@ -0,0 +1,7 @@
+VanillaTilt.init(document.querySelector(".project-box"), {
+  max: 10,
+  speed: 100,
+});
+
+//It also supports NodeList
+VanillaTilt.init(document.querySelectorAll(".project-box"));
